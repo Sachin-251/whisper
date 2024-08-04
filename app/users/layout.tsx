300e6f4008047b79c773b4132a2b2c58447642ef
Sachin-251 <sachin.251@outlook.com>
2024-08-05 02:00:22 +0530

    update 2

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,15 +1,20 @@
 import Sidebar from '@/components/sidebar/Sidebar';
 import React from 'react'
+import getUsers from '../actions/getUsers';
+import { UserList } from '@/sections/user';
 
 export default async function UsersLayout({
     children
 }:{
     children: React.ReactNode;
 }) {
+  const users = await getUsers();
+
   return (
     <Sidebar>
         <div className='h-full'>
-            {children}
+          <UserList items={users} />
+          {children}
         </div>
     </Sidebar>
   )
--- components/sidebar/DesktopItem.tsx
+++ components/sidebar/DesktopItem.tsx
@@ -22,8 +22,8 @@ const DesktopItem: React.FC<DesktopItemProps> = ({label, icon: Icon, href, onCli
   return (
     <li onClick={handleClick}>
         <Link href={href} className={clsx(
-            `group flex gap-x-3 rounded-md p-3 text-sm leading-6 font-semibold text-slate-400 hover:text-slate-200 hover:bg-secondary-bg/20`,
-            active && 'bg-secondary-bg/20 text-slate-200'
+            `group flex gap-x-3 rounded-md p-3 text-sm leading-6 font-semibold text-slate-400 hover:text-slate-200 hover:bg-secondary-bg/40`,
+            active && 'bg-secondary-bg/40 text-slate-200'
         )}>
             <Icon className='h-6 w-6 shrink-0' />
             <span className='sr-only'>{label}</span>
